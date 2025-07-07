acab9ee80ac770abc54e72acd184ffb3ac6e261c
umeeapkapiar <flyme3792@gmail.com>
2025-07-07 12:27:51 +0500

    MissCounterScreen.jsx

--- src/components/MissCounterScreen.jsx
+++ src/components/MissCounterScreen.jsx
@@ -91,7 +91,7 @@ export default function MissCounterScreen({ onNext, ...motionProps }) {
           transition={{ delay: 7.4 }}
           disabled={!showInfinity}
         >
-          Hear my heart💗
+          Dil Ki Awaaz Suno💗
         </motion.button>
       </div>
     </motion.div>
